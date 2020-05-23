32fe97d22966dba09211456985701f92dee9e0cc
Prem Prakash <prem.prakash@microsoft.com>
2020-05-23 01:36:59 -0700

    Update usage_details_list_python_sdk_example.py

--- Python_sdk/usage_details_list_python_sdk_example.py
+++ Python_sdk/usage_details_list_python_sdk_example.py
@@ -7,22 +7,41 @@ class MyUsage:
     def __init__(self, filePath):        
         with open(filePath) as f:
             credData = json.load(f)
-        self.scope = "subscriptions/" + credData['subscriptionId']                 
+        self.scope = "subscriptions/" + credData['subscriptionId']
         self.consumption_client = get_client_from_json_dict(ConsumptionManagementClient, credData)
 
-    def run(self, date_filter):
-        usages = self.consumption_client.usage_details.list(self.scope, filter=date_filter)   
+    def run_by_date(self, date_filter):
+        usages = self.consumption_client.usage_details.list(self.scope, filter=date_filter)
+        self.printresults(usages)
+
+    def run_by_billing_period(self, billing_period):
+        scope = self.scope + "/providers/Microsoft.Billing/billingPeriods/" + billing_period
+        usages = self.consumption_client.usage_details.list(self.scope);       
+        self.printresults(usages)
+
+    def printresults(self, usages):
         output = list(usages)             
         minDate = min(output, key=lambda x: x.date_property).date_property
         maxDate = max(output, key=lambda x: x.date_property).date_property
         print("Minimum Date:", minDate, ", Maximum Date:", maxDate)
 
 
-def run_example():
-    azure_usage = MyUsage("C:\SDK\Python\credentials.json")
-    date_filter = "properties/usageStart ge '2020-02-25' AND properties/usageStart lt '2020-02-27'"     
-    azure_usage.run(date_filter)    
+def run_example_by_datefilter():
+    azure_usage = MyUsage("credentials.json")
+    date_filter = "properties/usageStart ge '2020-02-26' AND properties/usageStart lt '2020-02-27'"     
+    print("Get data by date range '2020-02-26' and '2020-02-27'")
+    azure_usage.run_by_date(date_filter)    
+    print("Done")
+
+
+def run_example_by_billingperiod():
+    azure_usage = MyUsage("credentials.json")    
+    print("Get data by billing period 202003")
+    azure_usage.run_by_billing_period("202003")
+    print("Done")
 
 
 if __name__ == "__main__":
-    run_example()
+    run_example_by_datefilter()
+    run_example_by_billingperiod()
+
